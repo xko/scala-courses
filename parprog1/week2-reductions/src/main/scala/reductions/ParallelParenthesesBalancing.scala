b0d4ae4b7cb479f4c20c3f82d45247d48a80e622
Niko Ugarov <xxkkoo@gmail.com>
2021-06-11 22:39:33 +0200

    parprog1/week2: ex2: parenthesis balancing

--- parprog1/week2-reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ parprog1/week2-reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -3,6 +3,8 @@ package reductions
 import scala.annotation._
 import org.scalameter._
 
+import scala.collection.immutable.ArraySeq
+
 object ParallelParenthesesBalancingRunner {
 
   @volatile var seqResult = false
@@ -40,22 +42,41 @@ object ParallelParenthesesBalancing extends ParallelParenthesesBalancingInterfac
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    @tailrec
+    def bal(b: Int, chars: Seq[Char]):Int = if (b<0 || chars.isEmpty) b else
+        chars.head match {
+          case '(' => bal(b + 1, chars.tail)
+          case ')' => bal(b - 1, chars.tail)
+          case _  => bal(b, chars.tail)
+        }
+
+    bal(0, ArraySeq.unsafeWrapArray(chars)) == 0
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    @tailrec
+    def traverse(idx: Int, until: Int, unclosed: Int, unopened: Int) : (Int, Int) = chars(idx) match {
+      case _ if idx == until   => (unclosed, unopened)
+      case '(' if unopened > 0 => traverse(idx+1, until, unclosed, unopened-1)
+      case '('                 => traverse(idx+1, until, unclosed+1, unopened)
+      case ')' if unclosed >0  => traverse(idx+1, until, unclosed-1, unopened)
+      case ')'                 => traverse(idx+1, until, unclosed, unopened+1)
+      case _                   => traverse(idx+1, until, unclosed, unopened)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int) : (Int, Int) = {
+      if(until - from < threshold) traverse(from, until, 0, 0)
+      else {
+        val mid = from + (until - from) / 2
+        val ((ucl, uol),(ucr, uor)) = parallel(reduce(from, mid), reduce(mid, until))
+        ((ucl - uor)+ucr,uol+(uor - ucl))
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0,0)
   }
 
   // For those who want more:
