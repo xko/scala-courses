81fdc7fe5ad867afa45bb9bafd3bde74bf2917fb
Niko Ugarov <xxkkoo@gmail.com>
2021-09-30 00:45:21 +0200

    capstone/week1: Removing spark - week1 done

--- capstone/observatory/src/main/scala/observatory/Extraction.scala
+++ capstone/observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,19 @@
 package observatory
 
+import java.io.FileNotFoundException
+import java.nio.file.{Files, Paths}
 import java.time.LocalDate
+import scala.io.Source
+import scala.reflect.ClassTag
+import scala.util.Try
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction extends ExtractionInterface {
+  case class RawTempRec(stn: Long, wban: Long, month: Int, day: Int, temp: Double)
+  case class RawStationRec(stn: Long, wban: Long, lat: Double, lon: Double)
+
 
   /**
     * @param year             Year number
@@ -14,8 +22,17 @@ object Extraction extends ExtractionInterface {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    import SparkImpl._
-    locTemps(year, readStations(stationsFile), readTemps(temperaturesFile)).collect()
+    val stations = PlainImpl.readStations(stationsFile).groupBy(s => (s.stn,s.wban) ).mapValues(_.head)
+    val temps = PlainImpl.readTemps(temperaturesFile)
+    temps.groupBy(t => (t.stn,t.wban) ).mapValues { recs =>
+      for {
+        trec <- recs
+        srec <- stations.get((trec.stn,trec.wban))
+        date = LocalDate.of(year,trec.month,trec.day)
+        loc = Location(srec.lat,srec.lon)
+      } yield (date,loc,trec.temp)
+    }.values.flatten
+
   }
 
   /**
@@ -23,16 +40,58 @@ object Extraction extends ExtractionInterface {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    import SparkImpl._
-    import Spark.spark.implicits._
-    avgTemps(records.toSeq.toDS()).collect()
+    records.par.groupBy(_._2).mapValues(_.map(_._3)).mapValues { temps =>
+      val (count,sum) = temps.aggregate(0 -> 0.0)(
+        (agg, t) => (agg._1 + 1) -> (agg._2 + t),
+        (agg, bgg) => (agg._1 + bgg._1) -> (agg._2 + bgg._2)
+      )
+      sum/count
+    }.seq
+  }
+
+
+  object PlainImpl {
+    def fromCVS[T:ClassTag](name:String)(parse: Array[String] => Iterable[T] ): Array[T] = {
+      val src: Source = if( Files.isReadable(Paths.get(name))) Source.fromFile(name) else {
+        val stream = Source.getClass.getResourceAsStream(name)
+        if (stream == null) throw new FileNotFoundException(name)
+        Source.fromInputStream(stream)
+      }
+      try src.getLines().map(_.split(',').map(_.trim)).flatMap(parse).toArray
+      finally src.close()
+    }
+
+    def readStations(file: String): Array[RawStationRec] = fromCVS(file) {
+      case Array(stn, wban, lat, lon) if stn.nonEmpty || wban.nonEmpty =>
+        for {
+          stn  <- Try(if (stn.nonEmpty) stn.toLong else 0).toOption
+          wban <- Try(if (wban.nonEmpty) wban.toLong else 0).toOption
+          lat  <- Try(lat.toDouble).toOption
+          lon  <- Try(lon.toDouble).toOption
+        } yield RawStationRec(stn, wban, lat, lon)
+      case _ => None
+    }
+
+    def readTemps(file:String): Array[RawTempRec] = fromCVS(file) {
+      case Array(stn, wban, month, day, temp) if stn.nonEmpty || wban.nonEmpty =>
+        for {
+          stn <- Try(if (stn.nonEmpty) stn.toLong else 0).toOption
+          wban <- Try(if (wban.nonEmpty) wban.toLong else 0).toOption
+          month <- Try(month.toInt).toOption if month > 0 && month <= 12
+          day <- Try(day.toInt).toOption if day > 0 && day <= 31
+          tempF <- Try(temp.toDouble).toOption
+          tempC = (tempF-32)*5/9
+        } yield RawTempRec(stn, wban, month, day, tempC)
+      case _ => None
+    }
+
+
   }
 
   object SparkImpl {
+    import Spark._
     import org.apache.spark.sql._
     import org.apache.spark.sql.functions._
-
-    import Spark._
     import spark.implicits._
 
     def avgTemps(t: Dataset[(LocalDate, Location, Temperature)]): Dataset[(Location, Temperature)] = {
--- capstone/observatory/src/test/scala/observatory/ExtractionTest.scala
+++ capstone/observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -30,21 +30,20 @@ class ExtractionTest extends AnyFunSpec with Matchers {
 
   }
 
-  describe("spark implementation"){
-    import SparkImpl._
+  describe("implementation"){
+    import PlainImpl._
     it("reads temperatures") {
-      val d = readTemps("src/test/resources/temps.csv")
-      d.collect() should equal(Array(
+      readTemps("src/test/resources/temps.csv") should contain only (
         RawTempRec(10010, 0, 1, 1, -4.888888888888889d),
         RawTempRec(1, 2, 1, 1, -4.888888888888889d),
         RawTempRec(0, 10010, 1, 1, 37.611111111111114d),
         RawTempRec(0, 10010, 12, 12, 37.05555555555556d),
         RawTempRec(1, 2, 12, 21, 37.05555555555556d),
-        ))
+        )
     }
-   it("reads stations") {
+    it("reads stations") {
       val d = readStations("src/test/resources/stations.csv")
-      d.collect() should contain only (
+      d should contain only (
         RawStationRec(10010,0,+69.293,+016.144),
         RawStationRec(10110,0,0,+031.500),
         RawStationRec(10090,0,+80.650,+025.000),
@@ -52,22 +51,7 @@ class ExtractionTest extends AnyFunSpec with Matchers {
         RawStationRec(1,2,+69.293,+014.144)
       )
     }
-    it("locates temperatures") {
-      val d = locTemps(2048, readStations("src/test/resources/stations.csv"), readTemps("src/test/resources/temps.csv"))
-      d.collect() should contain only  (
-        (LocalDate.of(2048,12,21),Location(69.293,14.144),37.05555555555556),
-        (LocalDate.of(2048,1,1),Location(69.293,14.144),-4.888888888888889),
-        (LocalDate.of(2048,1,1), Location(69.293,16.144),-4.888888888888889)
-      )
-    }
 
-    it("averages temperatures") {
-      val d = avgTemps(locTemps(2048, readStations("src/test/resources/stations.csv"), readTemps("src/test/resources/temps.csv")))
-      d.collect() should contain only (
-        (Location(69.293, 16.144),-4.888888888888889),
-        (Location(69.293, 14.144),16.083333333333336)
-      )
-    }
   }
 
 }
