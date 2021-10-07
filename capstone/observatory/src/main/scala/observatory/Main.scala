9432f4b03b50a9300638b781748246a82736b196
Niko Ugarov <xxkkoo@gmail.com>
2021-10-08 01:00:47 +0200

    capstone/week3: Main class to generate images

--- capstone/observatory/src/main/scala/observatory/Main.scala
+++ capstone/observatory/src/main/scala/observatory/Main.scala
@@ -1,6 +1,25 @@
 package observatory
 
+import com.sksamuel.scrimage.nio.ImageWriter
+import observatory.Extraction.{locateTemperatures, locationYearlyAverageRecords}
+import observatory.Interaction.tile
+
+import java.nio.file.{Files, Paths}
+
 object Main extends App {
+  val ye = 1975
+
+  def img(year: Year, refs: Iterable[(Location, Temperature)])(t: Tile) = {
+    println(s"Generating $year: $t")
+    val image = tile(refs, Visualization.Colors, t)
+    val path = Paths.get(s"target/temperatures/$year/${t.zoom}/${t.x}-${t.y}.png")
+    Files.createDirectories(path.getParent)
+    image.output(path)(ImageWriter.default)
+  }
+
+  val temps = locateTemperatures(ye, "src/main/resources/stations.csv", s"src/main/resources/$ye.csv")
+  val refs: Iterable[(Location, Temperature)] = locationYearlyAverageRecords(temps)
+  img(ye,refs)(Tile(0,0,0))
 
 
 }
