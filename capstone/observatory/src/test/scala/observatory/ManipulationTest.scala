58f42934f08e7a32e5683c762aa2355108be9e19
Niko Ugarov <xxkkoo@gmail.com>
2021-10-08 22:14:27 +0200

    capstone/week4: some tests for Manipulation

--- capstone/observatory/src/test/scala/observatory/ManipulationTest.scala
+++ capstone/observatory/src/test/scala/observatory/ManipulationTest.scala
@@ -1,10 +1,32 @@
 package observatory
 
-import org.junit.Test
+import observatory.Manipulation._
+import org.scalacheck.Gen._
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-trait ManipulationTest extends MilestoneSuite {
-  private val milestoneTest = namedMilestoneTest("data manipulation", 4) _
+class ManipulationTest extends AnyFunSpec with Matchers with ScalaCheckPropertyChecks with TestUtil  {
 
-  // Implement tests for methods of the `Manipulation` object
+  describe("temperatures grid"){
+    val temps = chooseNum(-100.0, 100.0)
+    val refs = listOfN(5, zip(locations, temps))
+    val glocs = zip(chooseNum(-89,90),chooseNum(-180,179)).map(GridLocation.tupled)
+
+    it("equals at exact location"){
+      forAll(refs, glocs ,temps){ (refs, testGLoc, testTemp) =>
+        val g = makeGrid(testGLoc.loc -> testTemp :: refs)
+        g(testGLoc) shouldBe testTemp
+      }
+    }
+
+    it("averages at exact location"){
+      forAll( listOfN(20,zip(refs,temps)), glocs ){ (refss,testGLoc) =>
+        val g = average( refss.map{ case (refs,testTemp)=> testGLoc.loc -> testTemp :: refs } )
+        val expectedAvg = refss.map(_._2).sum / refss.size
+        g(testGLoc) shouldBe expectedAvg
+      }
+    }
+  }
 
 }
--- capstone/observatory/src/test/scala/observatory/TestUtil.scala
+++ capstone/observatory/src/test/scala/observatory/TestUtil.scala
@@ -9,10 +9,12 @@ import scala.math.Numeric.Implicits._
 import scala.reflect.ClassTag
 
 trait TestUtil {
-  implicit val arbLocation: Arbitrary[Location] = Arbitrary( for {
+  val locations: Gen[Location] = for {
     lat <- Gen.choose(-90.0, 90.0)
     lon <- Gen.choose(-180.0, 180.0)
-  } yield Location(lat, lon) )
+  } yield Location(lat, lon)
+
+  implicit val arbLocation: Arbitrary[Location] = Arbitrary(locations)
 
   case class beCloserTo[T : Numeric : ClassTag](near: T){
     def thenTo(far: T): Matcher[T] = Matcher[T] { x =>
