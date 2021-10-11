723e52bab6baf04fe612745abaf9bb52ac740faf
Niko Ugarov <xxkkoo@gmail.com>
2021-10-11 13:57:11 +0200

    capstone/week3: Interaction fixed to use correct colors

--- capstone/observatory/src/main/scala/observatory/Interaction.scala
+++ capstone/observatory/src/main/scala/observatory/Interaction.scala
@@ -1,7 +1,7 @@
 package observatory
 
 import com.sksamuel.scrimage.Image
-import observatory.Visualization.{Colors, pPredictTemperature, render}
+import observatory.Visualization.{pPredictTemperature, render}
 
 /**
   * 3rd milestone: interactive visualization
@@ -21,7 +21,7 @@ object Interaction extends InteractionInterface {
     * @return A 256×256 image showing the contents of the given tile
     */
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image =
-    render(tile.pixLocs, Colors, 256, 127)(pPredictTemperature(temperatures.par))
+    render(tile.pixLocs, colors, 256, 127)(pPredictTemperature(temperatures.par))
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
